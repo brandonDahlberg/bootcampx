1b4a166b12c113ec9c0ddfc10617d49abba7ddc4
brandonDahlberg <brandon.dahlberg21@gmail.com>
2022-02-08 22:44:20 +0000

    13 new query files

--- 4_more_queries/query.sql
+++
@@ -1,9 +0,0 @@
-SELECT assignments.name
-FROM assignments
-WHERE id NOT IN 
-(
-  SELECT assignment_id
-  FROM assignment_submissions
-  JOIN students ON students.id = student_id
-  WHERE students.name = 'Ibrahim Schimmel'
-);
